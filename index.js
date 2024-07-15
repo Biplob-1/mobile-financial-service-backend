85302167376b4b49130288a6633412f0363acdee
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-07-16 03:42:35 +0600

    user insert api add

--- index.js
+++ index.js
@@ -26,14 +26,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const userCollection = client.db("mfs").collection("users")
+
+    // users insert api
+    app.post('/insertUser', async (req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
